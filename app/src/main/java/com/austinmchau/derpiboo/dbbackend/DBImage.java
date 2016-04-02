147eb3ca95af15965b7352a352c19e34ea54aeba
austinmchau <austin.matthew.chau@gmail.com>
2016-04-01 17:43:40 -0700

    First commit

---
+++ app/src/main/java/com/austinmchau/derpiboo/dbbackend/DBImage.java
@@ -0,0 +1,53 @@
+package com.austinmchau.derpiboo.dbbackend;
+
+import android.media.Image;
+import android.util.Log;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Austin on 4/1/16.
+ */
+public class DBImage {
+
+    public final static String TAG = "Derpiboo";
+
+    public static class Representations {
+        URL thumb_tiny;
+        URL thumb_small;
+        URL thumb;
+        URL small;
+        URL medium;
+        URL large;
+        URL tall;
+        URL full;
+    }
+
+    //
+    //Mark: Data Fields
+    //
+
+    int id_number;
+    URL imageURL; //Nullable
+    Representations representations;
+    public URL thumbnail() {
+        return representations.thumb;
+    }
+
+    DBImage(int id_number, String imageURLString) {
+        this.id_number = id_number;
+        try {
+            this.imageURL = new URL("http:" + imageURLString);
+        } catch (MalformedURLException error) {
+            Log.d(TAG, error.toString());
+        }
+    }
+
+    //
+    //Mark: Data Store
+    //
+
+    Image image;
+
+}
